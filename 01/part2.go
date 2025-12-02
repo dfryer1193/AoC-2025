4bcc2c93621f669c0c55d68fecd927c4f9a8fd7f
David Fryer <dfryer1193@gmail.com>
2025-12-01 21:20:13 -0500

    Wrap up part 2

--- 01/part2.go
+++ 01/part2.go
@@ -28,29 +28,40 @@ func main() {
 			log.Fatal(err)
 		}
 
-		if direction == 'L' {
-			steps *= -1
+		var newPos int
+		var count int
+		if direction == 'R' {
+			newPos, count = rotateRight(position, steps)
+		} else if direction == 'L' {
+			newPos, count = rotateLeft(position, steps)
 		}
-
-		newPosition := position + steps
-
-		zeroPasses := 0
-		if position < 0 {
-			zeroPasses = (position / 100) * -1
-		} else if position > 99 {
-			zeroPasses = position / 100
-		}
-
-		position %= 100
-		if position < 0 {
-			position += 100
-		}
-
-		zeroCount += zeroPasses
-		if position == 0 {
-			zeroCount++
-		}
+		zeroCount += count
+		position = newPos
 	}
 
 	fmt.Println(zeroCount)
+}
+
+func rotateRight(position, steps int) (int, int) {
+	newPos := position + steps
+	fullRotations := steps / 100
+	remainder := steps % 100
+
+	if position+remainder > 99 {
+		fullRotations++
+	}
+
+	return newPos % 100, fullRotations
+}
+
+func rotateLeft(position, steps int) (int, int) {
+	newPos := position - steps
+	fullRotations := steps / 100
+	remainder := steps % 100
+
+	if remainder >= position && position != 0 {
+		fullRotations++
+	}
+
+	return ((newPos % 100) + 100) % 100, fullRotations
 }
