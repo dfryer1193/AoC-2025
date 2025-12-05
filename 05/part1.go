8e8616c4265d12c0a13ba5947f9a0318d9e9c342
David Fryer <dfryer1193@gmail.com>
2025-12-05 12:14:51 -0500

    D5P1 - Solution

---
+++ 05/part1.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type parseState int
+
+const (
+	parsingFreshnessRanges parseState = iota
+	parsingItemIDs
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) < 1 {
+		fmt.Println("Please provide an input filename.")
+		return
+	}
+
+	filename := args[0]
+	f, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer f.Close()
+
+	parserState := parsingFreshnessRanges
+	ranges := make([][2]int, 0)
+	freshItemCount := 0
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			parserState = parsingItemIDs
+			continue
+		}
+
+		switch parserState {
+		case parsingFreshnessRanges:
+			min, max, err := parseFreshnessRange(line)
+			if err != nil {
+				fmt.Println("Error parsing freshness range:", err)
+				return
+			}
+			fmt.Printf("Parsed freshness range: min=%d, max=%d\n", min, max)
+			ranges = append(ranges, [2]int{min, max})
+		case parsingItemIDs:
+			id, err := parseItemID(line)
+			if err != nil {
+				fmt.Println("Error parsing item ID:", err)
+				return
+			}
+			fmt.Printf("Parsed item ID: %d\n", id)
+			if isFresh(id, ranges) {
+				fmt.Printf("Item ID %d is fresh.\n", id)
+				freshItemCount++
+			}
+		}
+	}
+
+	fmt.Printf("Total fresh items: %d\n", freshItemCount)
+}
+
+func parseFreshnessRange(line string) (int, int, error) {
+	var min, max int
+	_, err := fmt.Sscanf(line, "%d-%d", &min, &max)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return min, max, nil
+}
+
+func parseItemID(line string) (int, error) {
+	var id int
+	_, err := fmt.Sscanf(line, "%d", &id)
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+func isFresh(id int, ranges [][2]int) bool {
+	for _, r := range ranges {
+		if !(id < r[0] || id > r[1]) {
+			return true
+		}
+	}
+
+	return false
+}
